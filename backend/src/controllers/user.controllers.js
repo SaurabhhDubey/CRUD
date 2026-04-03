f8714fe831e6f81910fe7a01a0cb63fa8f99d591
Saurabh Dubey <saurabhdubey3712@gmail.com>
2026-04-03 21:47:31 +0530

    controllers

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -21,3 +21,35 @@ catch(error){
     res.status(500).json({message:"reading error :" , error});
 }
 };
+
+
+export const updateUser = async (req, res)=>{
+    try{
+        const{id} = req.params;
+        const {username , tag} = req.body;
+         
+        const user = await User.findById(id);
+        if(!user){return res.status(400).json({message:"user not found"});}
+
+        if(username) user.username = username;
+        if(tag) user.tag = tag;
+        await user.save();
+        res.status(201).json({message:"user updated successfully" , user});
+    }
+    catch(error){
+        res.status(500).json({message:"update error" , error})
+    }
+};
+
+
+export const deleteUser = async (req , res)=>{
+    try{
+        const{id}=req.params;
+        const user = await User.findByIdAndDelete(id);
+        if(!user){return res.status(401).json({message:"user not found for delete"});}
+        return res.status(201).json({message:"user deleted successfully " , user});
+    }
+    catch(error){
+        res.status(500).json({message:"delete error" , error})
+    }
+};
--- backend/src/routers/routers.js
+++ backend/src/routers/routers.js
@@ -1,9 +1,12 @@
-import { registerUser } from "../controllers/user.controllers.js";
+import { registerUser ,read , updateUser , deleteUser } from "../controllers/user.controllers.js";
 import express from "express";
 const router = express.Router();
 
 
 router.post("/register" , registerUser);
+router.get("/read" , read);
+router.put("/update" , updateUser);
+router.delete("/delete" , deleteUser);
 
 
 export default router;      
